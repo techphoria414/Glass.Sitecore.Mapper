851feba8be28bf860b15492bc2dd02574274423a
Mike Edwards <mikeedwards83@googlemail.com>
2012-02-24 22:16:26 +0000

    test start

---
+++ Source/Glass.Sitecore.Mapper.Razor.Tests/RenderingTypes/DynamicRazorRenderingTypeFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glass.Sitecore.Mapper.Razor.RenderingTypes;
+
+namespace Glass.Sitecore.Mapper.Razor.Tests.RenderingTypes
+{
+    [TestFixture]
+    public class DynamicRazorRenderingTypeFixture
+    {
+        DynamicRazorRenderingType
+
+    }
+}
