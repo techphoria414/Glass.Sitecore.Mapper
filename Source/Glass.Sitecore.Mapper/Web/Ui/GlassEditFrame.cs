1896572cce48c5fc7266a6da20c942ccf01e4518
Mike Edwards <mikeedwards83@googlemail.com>
2012-07-22 22:29:05 +0100

    2.0.8.0

---
+++ Source/Glass.Sitecore.Mapper/Web/Ui/GlassEditFrame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Web.UI.WebControls;
+using System.Web;
+using System.Web.UI;
+using System.IO;
+
+namespace Glass.Sitecore.Mapper.Web.Ui
+{
+    public class GlassEditFrame : IDisposable
+    {
+        EditFrame _frame;
+        HtmlTextWriter _writer;
+        public GlassEditFrame(string buttons, HttpContext context)
+        {
+            _frame = new EditFrame();
+            _frame.Buttons = buttons;
+            _writer = new HtmlTextWriter(context.Response.Output);
+
+        }
+        public void RenderFirstPart()
+        {
+            _frame.RenderFirstPart(_writer);
+        }
+
+
+
+        public void Dispose()
+        {
+            _frame.RenderLastPart(_writer);
+        }
+    }
+}
