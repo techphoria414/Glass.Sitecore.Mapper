f0eab0d5e6b7bbae5570e979db0c0847ed3c8bec
Mike Edwards <mikeedwards83@googlemail.com>
2012-08-24 07:34:28 +0100

    MVC GetModel Processor

---
+++ Source/Glass.Sitecore.Mapper/Pipelines/Response/GetModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Mvc.Pipelines.Response.GetModel;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Extensions;
+using Sitecore.Mvc.Presentation;
+
+namespace Glass.Sitecore.Mapper.Pipelines.Response
+{
+    public class GetModel : GetModelProcessor
+    {
+
+        public override void Process(GetModelArgs args)
+        {
+            if (args.Result == null)
+            {
+
+                try
+                {
+                    Type type = GetFromField(args.Rendering, args);
+                    if (Context.StaticContext.Classes.ContainsKey(type))
+                    {
+                        ISitecoreContext context = new SitecoreContext();
+                        var result = context.GetCurrentItem(type);
+                        args.Result = result;
+                    }
+                }
+                catch (MapperException ex)
+                {
+                    //do nothing
+                }
+                catch (System.TypeLoadException ex)
+                {
+                    //do nothing
+                }
+            }
+        }
+
+        protected virtual Type GetFromField(Rendering rendering, GetModelArgs args)
+        {
+            Item obj = ObjectExtensions.ValueOrDefault<RenderingItem, Item>(rendering.RenderingItem, (Func<RenderingItem, Item>)(i => i.InnerItem));
+            if (obj == null)
+                throw new MapperException("No rendering item");
+            else
+                return Type.GetType(obj["Model"], true);
+        }
+    }
+}
